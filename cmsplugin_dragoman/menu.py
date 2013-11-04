4bf996ac3b34536486c24c17613a6a4e7af66b8b
Vladimir Bolshakov <vovanbo@gmail.com>
2013-11-04 23:51:44 +0400

    Add filter by language for menu items. Add limit for menu items which can be set by CMSPLUGIN_DRAGOMAN_MENU_ITEMS_LIMIT in settings.py. Default is 5.

--- cmsplugin_dragoman/menu.py
+++ cmsplugin_dragoman/menu.py
@@ -3,7 +3,12 @@ from menus.base import Menu, NavigationNode
 from menus.menu_pool import menu_pool
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import get_language
 from dragoman_blog.models import Entry, EntryTranslation
+from django.conf import settings
+
+CMSPLUGIN_DRAGOMAN_MENU_ITEMS_LIMIT = getattr(
+    settings, 'CMSPLUGIN_DRAGOMAN_MENU_ITEMS_LIMIT', 5)
 
 
 class BlogEntriesMenu(CMSAttachMenu):
@@ -11,7 +16,7 @@ class BlogEntriesMenu(CMSAttachMenu):
 
     def get_nodes(self, request):
         nodes = []
-        for entry in EntryTranslation.objects.filter(is_published=True):
+        for entry in EntryTranslation.objects.filter(is_published=True, language_code=get_language()).order_by('-pub_date')[:CMSPLUGIN_DRAGOMAN_MENU_ITEMS_LIMIT]:
             node = NavigationNode(
                 entry.title,
                 reverse('dragoman_blog_detail',
