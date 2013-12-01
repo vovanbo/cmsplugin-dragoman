d90a291fbf92a00eb2ac6bb402dee9c25392cf5f
Vladimir Bolshakov <vovanbo@gmail.com>
2013-12-01 12:31:55 +0400

    Add tags for published posts with current language to django cms menus.

--- cmsplugin_dragoman/menu.py
+++ cmsplugin_dragoman/menu.py
@@ -3,7 +3,8 @@ from menus.base import NavigationNode
 from menus.menu_pool import menu_pool
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
-from dragoman_blog.models import EntryTranslation
+from dragoman_blog.models import EntryTranslation, TranslationTagged
+from django.utils.translation import get_language
 
 
 class BlogEntriesMenu(CMSAttachMenu):
@@ -11,11 +12,16 @@ class BlogEntriesMenu(CMSAttachMenu):
 
     def get_nodes(self, request):
         nodes = []
-        for tag in EntryTranslation.tags.all():
+        posts = EntryTranslation.objects.filter(
+            is_published=True, language_code=get_language())
+        tags = TranslationTagged.objects.none()
+        for post in posts:
+            tags = tags | TranslationTagged.tags_for(EntryTranslation, post).order_by("name")
+        for tag in tags:
             node = NavigationNode(
                 tag.name,
                 reverse(
-                    'dragoman_blog_list_by_tag', kwargs={'tag': tag.name}),
+                    'dragoman_blog_list_by_tag', kwargs={'tag': tag.slug}),
                 tag.pk
             )
             nodes.append(node)
