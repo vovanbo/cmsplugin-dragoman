c084898e1e3af46ef28856ea9eab5e1f3f3d3880
Vladimir Bolshakov <vovanbo@gmail.com>
2013-12-01 12:32:26 +0400

    Clean cache of django cmd menus when new blog Entry added.

--- cmsplugin_dragoman/dragoman_models.py
+++ cmsplugin_dragoman/dragoman_models.py
@@ -1,6 +1,9 @@
 from django.utils.translation import ugettext as _
 from dragoman_blog.model_bases import BaseEntry
 from cms.models.fields import PlaceholderField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from menus.menu_pool import menu_pool
 
 
 class Entry(BaseEntry):
@@ -12,3 +15,8 @@ class Entry(BaseEntry):
         verbose_name_plural = _('Entrys')
         abstract = False
         app_label = 'cmsplugin_dragoman'
+
+
+@receiver(post_save, sender=Entry)
+def clear_menu_pool_cache(sender, **kwargs):
+    menu_pool.clear()
